82b39275fa15a075db80ffd955cc06fb5ad8cf93
tinynakji <tinynakji@gmail.com>
2023-06-15 08:18:45 -0700

    Make backend runnable

--- application/backend/application/entrypoint.sh
+++ application/backend/application/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # gunicorn --chdir --bind 0.0.0.0:5000 wsgi:app
-( cd /usr/src/app/flask_app ; alembic upgrade head )
+# ( cd /usr/src/app/flask_app ; alembic upgrade head )
 gunicorn --bind 0.0.0.0:5000 wsgi:app
