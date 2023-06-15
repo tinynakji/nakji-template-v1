c9d04c2e4b8eef8ce0d974625a9b35cc6245bf96
tinynakji <tinynakji@gmail.com>
2023-06-15 07:53:43 -0700

    Docker push to ECR in build script

--- deploy/build_images.sh
+++ deploy/build_images.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-docker build -t nakji_deploy_api:latest ../application/backend
+COMMIT_HASH=$(git rev-parse --short HEAD)
+
+BACKEND_ECR_REPO="370056820766.dkr.ecr.us-east-1.amazonaws.com/tinynakji-backend"
+docker build \
+    -t ${BACKEND_ECR_REPO}:${COMMIT_HASH} \
+    -t ${BACKEND_ECR_REPO}:latest \
+        ../application/backend
+docker push ${BACKEND_ECR_REPO}:${COMMIT_HASH}
+docker push ${BACKEND_ECR_REPO}:latest
 
 # List of build args needed for UI Docker image build
 
-docker build -t nakji_deploy_ui:latest ../application/frontend
+FRONTEND_ECR_REPO="370056820766.dkr.ecr.us-east-1.amazonaws.com/tinynakji-frontend"
+docker build \
+    -t ${FRONTEND_ECR_REPO}:${COMMIT_HASH} \
+    -t ${FRONTEND_ECR_REPO}:latest \
+     ../application/frontend
+docker push ${FRONTEND_ECR_REPO}:${COMMIT_HASH}
+docker push ${FRONTEND_ECR_REPO}:latest
