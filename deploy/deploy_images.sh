7c08c211752784bb7242d47cf636c72bdbba679a
tinynakji <tinynakji@gmail.com>
2023-06-16 08:54:42 -0700

    Compile kube files from env file

--- deploy/deploy_images.sh
+++ deploy/deploy_images.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-kubectl apply \
-    -f kube/backend/deployment.yaml \
-    -f kube/backend/service.yaml \
-    -f kube/frontend/deployment.yaml \
-    -f kube/frontend/service.yaml
+################
+# Get ENV VARS #
+################
+export $(grep -v '^#' ../.env | xargs)
+
+envsubst < kube/backend/deployment-template.yaml > kube/backend/deployment.yaml
+envsubst < kube/backend/service-template.yaml > kube/backend/service.yaml
+envsubst < kube/frontend/deployment-template.yaml > kube/frontend/deployment.yaml
+envsubst < kube/frontend/service-template.yaml > kube/frontend/service.yaml
+
+envsubst < kube/ingress/ingress-template.yaml > kube/ingress/ingress.yaml
+
+# kubectl apply \
+#     -f kube/backend/deployment.yaml \
+#     -f kube/backend/service.yaml \
+#     -f kube/frontend/deployment.yaml \
+#     -f kube/frontend/service.yaml
